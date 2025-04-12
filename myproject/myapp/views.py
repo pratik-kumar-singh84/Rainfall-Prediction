a394d0046649b8ebf7e671b1a0cd31406fb46481
PRATIK KUMAR SINGH <pratikkumarsingh000@gmail.com>
2025-04-12 16:30:28 +0530

    add files

---
+++ myproject/myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import pickle
+import os
+# Create your views here.
+def index(req):
+    res=''
+    if req.method=='POST':
+        pn=float(req.POST['pn'])
+        maxt=float(req.POST['maxt'])
+        mint=float(req.POST['mint'])
+        wind=float(req.POST['wind'])
+        path=os.path.dirname(__file__)
+        model=pickle.load(open(os.path.join(path,'rfp.pkl'),'rb'))
+        X=[pn,maxt,mint,wind]
+        res=str(model.predict([X])[0])
+    return render(req,"index.html",{'res':res})
